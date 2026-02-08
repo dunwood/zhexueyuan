e3be9b2918277881ebe81ec0183d3ce28087c666
dunwood <zhouyahui@126.com>
2026-02-08 23:17:08 +0800

    Delete test_python.bat

--- test_python.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo 正在测试... > test_output.txt
-echo 当前目录: %cd% >> test_output.txt
-C:\Users\Admin\AppData\Local\Programs\Python\Python312\python.exe --version >> test_output.txt 2>&1
-echo 返回代码: %errorlevel% >> test_output.txt
-echo 测试完成 >> test_output.txt
-notepad test_output.txt
